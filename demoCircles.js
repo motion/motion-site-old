74415e676d3d4ee28305056bc92f0f16f962bee3
natew <natewienert@gmail.com>
2015-09-01 11:31:11 -0700

    tweaks

--- demoCircles.js
+++ demoCircles.js
@@ -3,10 +3,10 @@ import { Spring } from 'react-motion'
 view DemoCircles {
   range = i => Array.apply(i,Array(i)).map((_, i) => i)
 
-  @num = 0
+  @num = 2
 
   <title>Num circles: {@num}</title>
-  <input type="range" sync={@num} />
+  <input type="range" onChange={e => @num = e.target.value} />
   <circles>
     <Circle repeat={range(@num)} />
   </circles>
@@ -16,22 +16,27 @@ view DemoCircles {
     margin: 'auto',
     width: 300
   }
+
+  $circles = {
+    flexFlow: 'row'
+  }
 }
 
 view Circle {
   c = () => Math.round(Math.random()*255)
+  bg = `rgb(${c()}, ${c()}, ${c()})`
+
   circle = () => ({
     borderRadius: 100,
-    width: ^size || 200,
-    height: ^size || 200,
+    width: ^size || 20,
+    height: ^size || 20,
     flexShrink: 0,
-    margin: ['auto', -40],
+    margin: ['auto', 0],
     opacity: 0.5,
-
-    background: `rgb(${c()}, ${c()}, ${c()})`
+    background: bg
   })
 
-  <Spring defaultValue={0} endValue={100}>
+  <Spring defaultValue={0} endValue={10}>
     {i => <circle style={[circle(), { marginLeft: i }]} />}
   </Spring>
 
