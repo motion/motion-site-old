98bb152fb66898c11ebfa220c8df9ef41e57d93b
natew <natewienert@gmail.com>
2016-02-19 08:47:41 -0800

    examples

--- pages/examples.js
+++ pages/examples.js
@@ -1,30 +1,29 @@
 const examples = [
+  // {
+  //   title: 'Intro',
+  //   examples: [
+  //     { title: "Begin", view: 'Intro', slug: 'intro', description: `
+  //       Use multiple view together with styles.
+  //     ` },
+  //     {
+  //       title: "More styling", view: 'Fetch', slug: 'fetch', description: `
+  //       Mix together props, style, and lifecycle events.
+  //     ` },
+  //   ]
+  // },
   {
-    title: 'Introduction',
+    title: 'Intermediate',
     examples: [
-      { title: "Begin", view: 'Intro', slug: 'intro', description: `
+      { title: "Logic and Style", view: 'Intro', slug: 'intro', description: `
         Learn how to use views in Motion. Learn the complete style syntax, using variables
         for dynamic content, and basics of adding events.
       ` },
-      { title: "Routes/Fetch", view: 'Fetch', slug: 'fetch', description: `
+      { title: "Routes / Fetch", view: 'Fetch', slug: 'fetch', description: `
         Learn how to fetch data using fetch as well as async/await.
         This tutorial also teaches routing, and how to use the Motion router with views.
       ` },
     ]
   },
-  {
-    title: 'Integration',
-    examples: [
-      { title: "Begin", view: 'Intro', slug: 'intro', description: `
-        Learn how to use views in Motion. Learn the complete style syntax, using variables
-        for dynamic content, and basics of adding events.
-      ` },
-      { title: "Routing and Fetching", view: 'Fetch', slug: 'fetch', description: `
-        Learn how to fetch data using fetch as well as async/await.
-        This tutorial also teaches routing, and how to use the Motion router with views.
-      ` },
-    ]
-  }
 ]
 
 let active = 0
@@ -78,5 +77,8 @@ view Examples {
     {view.el(`Examples.${capitalize(params.slug)}`)}
   </Page>
 
-  $body = { width: '100%' }
+  $ = {
+    width: '100%',
+    padding: [40, 0]
+  }
 }
